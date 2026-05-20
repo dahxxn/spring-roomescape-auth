89b468a8bd1cfb9beeec076583c81a0dc54ddce6
dahxxn <dahyeonhan4529@gmail.com>
2026-05-20 14:53:07 +0900

    feat: TokenProvider의 필드 추가
    - SecretKey, Expiration 필드 추가
    - application.properties에 설정값 주입

---
+++ src/main/java/roomescape/auth/token/TokenProvider.java
@@ -0,0 +1,37 @@
+package roomescape.auth.token;
+
+import static io.jsonwebtoken.security.Keys.hmacShaKeyFor;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenProvider {
+    private final SecretKey secretKey;
+    private final long expirationTime;
+
+    public TokenProvider(
+            @Value("${jwt.secret}") String secretKey,
+            @Value("${jwt.expiration}") long expirationTime
+    ) {
+        this.secretKey = hmacShaKeyFor(secretKey.getBytes(UTF_8));
+        this.expirationTime = expirationTime;
+    }
+
+    public String createToken(Long memberId){
+        //TODO: memberId를 담은 JWT 생성
+        return null;
+    }
+
+    public Long extractMemberId(String token){
+        //TODO: 토큰 검증 후 memberId 추출
+        return null;
+    }
+
+    private boolean validateToken(String token){
+        //TODO: 유효한 토큰인지 확인
+        return false;
+    }
+}
